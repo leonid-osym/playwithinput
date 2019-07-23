6834197ff8dad43d33efe1ba90a601ab11259593
Leonid Osym <leonido@wizardsdev.com>
2019-07-23 09:48:35 +0300

    working version

--- App.js
+++ App.js
@@ -14,60 +14,83 @@ export default class App extends Component {
 
   processInputFromFirstField = (text) => {
     this.setState({ firstCurrencyFieldText: text })
-    //this.setState({secondCurrencyFieldText: text})
-    //this.formatString(text, 'en');
-    var r = this.getNumberFromString(text);
-    var rr = this.getStringFromNumber(r, 'en');
-    this.setState({secondCurrencyFieldText: r.toString()})
+    let lang = 'en';
+    let rrr = this.formatInput(text, lang);
+    let s1 = this.getNumberFromString(text, lang);
+    let s2 = this.getStringFromNumber(s1, lang);
+
+    this.setState({ firstCurrencyFieldText: rrr });
+    this.setState({ secondCurrencyFieldText: s2 });
   }
 
   processInputFromSecondField = (text) => {
-    this.setState({ secondCurrencyFieldText: text })
-    this.setState({ firstCurrencyFieldText: text })
+    let lang = 'en';
+    let rrr = this.formatInput(text, lang);
+    let s1 = this.getNumberFromString(text, lang);
+    let s2 = this.getStringFromNumber(s1, lang);
+
+    this.setState({ secondCurrencyFieldText: rrr });
+    this.setState({ firstCurrencyFieldText: s2 });
   }
 
-  getNumberFromString = (str) => {
-    let res = 0;
+  formatInput = (str, lang) => {
     let intPart = '';
     let fractPart = '';
-    if (str.includes('.')) {
-      let pos = str.indexOf('.');
-      intPart = str.slice(0, pos).replace(/[,' ']/g, '');
-      fractPart = '.'+ str.replace(/[,.' ']/g, '').slice(pos, pos + 3);
+    if (str.includes('.') && str.indexOf('.')) {
+      var pos = str.indexOf('.');
+      intPart = str.slice(0, pos).replace(/[,.' ']/g, '') + '.';
+      fractPart = str.slice(pos).replace(/[,.' ']/g, '').slice(0, 2);
     } else {
       intPart = str.replace(/[,.' ']/g, '');
     }
-    res = parseFloat(intPart + fractPart);
-    return res ? res : 0 ;
-  }
-
-  getStringFromNumber = (num, lang) => {
-    let str = num.toString();
-    let intPart = '';
-    let fractPart = '';
-    if (str.includes('.')) {
-      var pos = str.indexOf('.');
-      intPart = str.slice(0, pos);
-      fractPart = str.slice(pos, pos + 3);
-      let pattern = this.getRegexPattern(intPart.length, lang);
-
-      intPart = intPart.replace(new RegExp(pattern[0]), pattern[1]);
-      intPart = intPart[0] === (',' || ' ') ? intPart.slice(1) : intPart;
-      str = intPart + fractPart;
-    } else {
-      let pattern = this.getRegexPattern(str.length, lang);
-
-      intPart = str.replace(new RegExp(pattern[0]), pattern[1]);
-      intPart = intPart[0] === (',' || ' ') ? intPart.slice(1) : intPart;
-      str = intPart + fractPart;
-    }
+    let pattern = this.getRegexPattern(intPart.length - 1, lang);
+    intPart = intPart.replace(new RegExp(pattern[0]), pattern[1]);
+    intPart = intPart[0] === (',' || ' ') ? intPart.slice(1) : intPart;
+    str = intPart + fractPart;
     return str;
-    //var str = str.replace(/[,' ']/g,'');
-    //var newstr = str.replace('/([0-9]{1,3})([0-9]{3})([0-9]{1,2})/', "$1 $2.$3");
-    // var newstr = str.replace(/([0-9]{1,3})([0-9]{3})/, "$1 $2");
-    // this.setState({secondCurrencyFieldText: newstr})
-    //return 0;
   }
+
+  getNumberFromString = (str, lang) => {
+    str = this.formatInput(str, lang);
+    if (str && typeof str === 'string') {
+      let res = str.replace(/[,' ']/g, '');
+      return res ? res : 0;
+    }
+    return 0;
+  }
+
+  getStringFromNumber = (num, lang)=>{
+    let str = num.toString();
+    return this.formatInput(str, lang);
+  }
+
+  // getStringFromNumber = (num, lang) => {
+  //   let str = num.toString();
+  //   let intPart = '';
+  //   let fractPart = '';
+  //   if (str.includes('.')) {
+  //     var pos = str.indexOf('.');
+  //     intPart = str.slice(0, pos);
+  //     fractPart = str.slice(pos, pos + 3);
+  //     let pattern = this.getRegexPattern(intPart.length, lang);
+
+  //     intPart = intPart.replace(new RegExp(pattern[0]), pattern[1]);
+  //     intPart = intPart[0] === (',' || ' ') ? intPart.slice(1) : intPart;
+  //     str = intPart + fractPart;
+  //   } else {
+  //     let pattern = this.getRegexPattern(str.length, lang);
+
+  //     intPart = str.replace(new RegExp(pattern[0]), pattern[1]);
+  //     intPart = intPart[0] === (',' || ' ') ? intPart.slice(1) : intPart;
+  //     str = intPart + fractPart;
+  //   }
+  //   return str;
+  //   //var str = str.replace(/[,' ']/g,'');
+  //   //var newstr = str.replace('/([0-9]{1,3})([0-9]{3})([0-9]{1,2})/', "$1 $2 555555$3");
+  //   // var newstr = str.replace(/([0-9]{1,3})([0-9]{3})/, "$1 $2");
+  //   // this.setState({secondCurrencyFieldText: newstr})
+  //   //return 0;
+  // }
 
   getRegexPattern = (number, lang) => {
     let pat1 = ['([0-9]{0,3})'];
